bbf06b291f1d0ca52cc423ea13d4aca5a4afe6c0
Gabrielle Despaigne <Gdespaigne@yahoo.com>
2025-10-25 13:04:41 -0700

    lab4

--- index.js
+++ index.js
@@ -14,20 +14,27 @@ import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
   if (profileStats) {
     try {
       const githubData = await fetchGitHubData('gdespaigne'); 
-      console.log(githubData); // ok to keep for the lab
+      console.log(githubData);
 
       if (githubData && !githubData.message) {
         profileStats.innerHTML = `
-          <dl>
-            <dt>public repos</dt><dd>${githubData.public_repos}</dd>
-            <dt>public gists</dt><dd>${githubData.public_gists}</dd>
-            <dt>followers</dt><dd>${githubData.followers}</dd>
-            <dt>following</dt><dd>${githubData.following}</dd>
-          </dl>
+          <div class="stat">
+            <dt>Public Repos</dt><dd>${githubData.public_repos}</dd>
+          </div>
+          <div class="stat">
+            <dt>Public Gists</dt><dd>${githubData.public_gists}</dd>
+          </div>
+          <div class="stat">
+            <dt>Followers</dt><dd>${githubData.followers}</dd>
+          </div>
+          <div class="stat">
+            <dt>Following</dt><dd>${githubData.following}</dd>
+          </div>
         `;
       } else {
         profileStats.textContent = 'unable to load github stats right now.';
-      }
+      }      
+
     } catch (err) {
       console.error(err);
       profileStats.textContent = 'unable to load github stats right now.';
