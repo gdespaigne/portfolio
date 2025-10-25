fac31e671548e7d4c0dd1c62da61cdb598a943e6
Gabrielle Despaigne <Gdespaigne@yahoo.com>
2025-10-25 12:31:47 -0700

    lab4

--- projects/projects.js
+++ projects/projects.js
@@ -7,3 +7,8 @@ import { fetchJSON, renderProjects } from '../global.js';
   const projects = await fetchJSON('../lib/projects.json');
   renderProjects(projects, container, 'h2');
 })();
+
+const titleEl = document.querySelector('main h1') || document.querySelector('h1');
+if (titleEl) {
+  titleEl.textContent = `(${projects.length}) Projects`;
+}
