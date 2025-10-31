d84d318ee285ed4b8598f9a70aa60b8a2d7da3f3
Gabrielle Despaigne <Gdespaigne@yahoo.com>
2025-10-31 12:06:53 -0700

    lab5

--- global.js
+++ global.js
@@ -95,8 +95,10 @@ export function renderProjects(projects, containerElement, headingLevel = "h2") 
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
-      <p><span class="project-year">${project.year ?? ""}</span></p>
+      <div class="project-desc-wrap">
+        <p>${project.description}</p>
+        <div class="project-year">${project.year ?? ""}</div>
+      </div>
     `;
     containerElement.appendChild(article);
   });
